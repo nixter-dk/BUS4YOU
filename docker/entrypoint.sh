f7f91f7b71d68fd961c5bb87cd553a36a8701221
nixter-dk <nikster-dk@hotmail.com>
2026-08-18 14:00:29 +0200

    Enforce a single Apache MPM at startup

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,4 +6,13 @@ php artisan view:cache
 php artisan migrate --force
 php artisan db:seed --force
 
+rm -f \
+    /etc/apache2/mods-enabled/mpm_event.load \
+    /etc/apache2/mods-enabled/mpm_event.conf \
+    /etc/apache2/mods-enabled/mpm_worker.load \
+    /etc/apache2/mods-enabled/mpm_worker.conf \
+    /etc/apache2/mods-enabled/mpm_itk.load \
+    /etc/apache2/mods-enabled/mpm_itk.conf
+a2enmod mpm_prefork >/dev/null
+
 exec "$@"
